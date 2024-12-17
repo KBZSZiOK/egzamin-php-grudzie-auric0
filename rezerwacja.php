259dec863a2729a184b01455d9996123cc1ce1db
auric0 <156914049+auric0@users.noreply.github.com>
2024-12-17 09:33:37 +0100

    Add files via upload

---
+++ rezerwacja.php
@@ -0,0 +1,11 @@
+<?php
+
+if (isset($_POST['data']) && isset($_POST['ludzie']) && isset($_POST['tel'])) {
+
+    $data = $_POST['data'];
+    $ludzie = $_POST['ludzie'];
+    $tel = $_POST['tel'];
+
+}
+
+?>
