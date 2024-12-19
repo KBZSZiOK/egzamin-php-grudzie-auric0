a728cae111fdce837c2874d01045adf53ae9273a
auric0 <156914049+auric0@users.noreply.github.com>
2024-12-19 14:47:56 +0100

    i hate php

--- rezerwacja.php
+++ rezerwacja.php
@@ -6,6 +6,18 @@ if (isset($_POST['data']) && isset($_POST['ludzie']) && isset($_POST['tel'])) {
     $ludzie = $_POST['ludzie'];
     $tel = $_POST['tel'];
 
+    $server = 'localhost';
+    $user = 'root';
+    $password = '';
+    $database = 'baza1';
+
+    $connect = mysqli_connect($server, $user, $password, $database);
+    $zapytanie = 'INSERT INTO rezerwacje ("data_rez", "liczba_osob", "telefon") VALUES ("$data", "$ludzie", "$tel")';
+
+    mysqli_query($connect, $zapytanie);
+    mysqli_close($connect);
+    echo "Dodano rezerwację do bazy";
+
 }
 
 ?>
